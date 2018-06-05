0028401c74f7fed822a357e4e88b5120392d19aa
Sagar Chalise <chalisesagar@gmail.com>
2018-06-05 06:08:28 +0545

    :pencil: :sparkles: use black by default

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# peasy-yapf
-Python Code Formatting  for Geany IDE based on [Peasy](https://github.com/kugel-/peasy)
+# peasy-pyformat
+Python Code Formatting for Geany IDE based on [Peasy](https://github.com/kugel-/peasy)
 
-`pip install yapf`
+## Requires Any one formatter. I personally use black
+
+ - [Black](https://github.com/ambv/black) [If supported]
+`pip install black`
+
+
+ - [YAPF](https://github.com/google/yapf)
+``pip install yapf``
+
+
+
+#### [NOTE] Donot mix both formatter.
--- peasyyapf.py
+++ peasyyapf.py
@@ -2,12 +2,33 @@ import os
 from gi.repository import Gtk
 from gi.repository import Geany
 from gi.repository import Peasy
+
+# see if black is available
 try:
-    from yapf.yapflib.yapf_api import FormatCode  # reformat a string of code
-    from yapf.yapflib.file_resources import GetDefaultStyleForDir
+    import black
 except ImportError:
-    print('No YAPF available')
-    FormatCode = None
+    print("black formatter not available trying yapf")
+    try:
+        from yapf.yapflib.yapf_api import (
+            FormatCode
+        )  # reformat a string of code
+        from yapf.yapflib.file_resources import GetDefaultStyleForDir
+    except ImportError:
+        print("No YAPF available")
+        FormatCode = None
+else:
+    GetDefaultStyleForDir = None
+
+    def FormatCode(content, style_config=None):
+        try:
+            changed_content = black.format_file_contents(
+                content, line_length=79, fast=False
+            )
+        except black.NothingChanged:
+            return "", False
+        else:
+            return changed_content, True
+
 
 _ = Peasy.gettext
 
@@ -19,36 +40,44 @@ class YAPFPlugin(Peasy.Plugin):
 
     def do_enable(self):
         geany_data = self.geany_plugin.geany_data
-        self.item = Geany.ui_image_menu_item_new(Gtk.STOCK_EXECUTE,
-                                                 _("Format Python Code"))
+        self.item = Geany.ui_image_menu_item_new(
+            Gtk.STOCK_EXECUTE, _("Format Python Code")
+        )
         self.item.connect("activate", self.on_item_click)
         geany_data.main_widgets.tools_menu.append(self.item)
         self.item.show_all()
         self.keys = self.add_key_group("python_code_format", 1)
-        self.keys.add_keybinding("python_code_format", _("Python Code Format"),
-                                 self.item, 0, 0)
+        self.keys.add_keybinding(
+            "python_code_format", _("Python Code Format"), self.item, 0, 0
+        )
         return True
 
     def on_item_click(self, item=None):
         if not FormatCode:
             return
         cur_doc = Geany.document_get_current()
-        if not cur_doc or cur_doc.file_type.id != Geany.FiletypeID.FILETYPES_PYTHON:
+        if (
+            not cur_doc
+            or cur_doc.file_type.id != Geany.FiletypeID.FILETYPES_PYTHON
+        ):
             return
         sci = cur_doc.editor.sci
         contents = sci.get_contents(-1)
         if not contents:
             return
-        style = GetDefaultStyleForDir(os.path.dirname(cur_doc.real_path))
-        project = self.geany_plugin.geany_data.app.project
-        if project and not style:
-            style = GetDefaultStyleForDir(project.base_path)
+        style = None
+        if GetDefaultStyleForDir is not None:
+            style = GetDefaultStyleForDir(os.path.dirname(cur_doc.real_path))
+            project = self.geany_plugin.geany_data.app.project
+            if project and not style:
+                style = GetDefaultStyleForDir(project.base_path)
         format_text, formatted = FormatCode(contents, style_config=style)
         if formatted:
             sci.set_text(format_text)
             Geany.msgwin_clear_tab(Geany.MessageWindowTabNum.MESSAGE)
-            Geany.msgwin_msg_add_string(Geany.MsgColors.BLACK, -1, cur_doc,
-                                        "Code Formatted")
+            Geany.msgwin_msg_add_string(
+                Geany.MsgColors.BLACK, -1, cur_doc, "Code Formatted"
+            )
             Geany.msgwin_switch_tab(Geany.MessageWindowTabNum.MESSAGE, False)
 
     def do_disable(self):
