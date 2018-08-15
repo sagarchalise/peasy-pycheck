85dc49796c763e9b335692e5d60893c0318057c3
Sagar Chalise <chalisesagar@gmail.com>
2018-08-15 17:37:32 +0545

    :sparkles: configuration for formatter/linter chooser and line length

--- peasy-pycheck.py
+++ peasy-pycheck.py
@@ -7,28 +7,43 @@ from concurrent.futures import ThreadPoolExecutor
 from gi.repository import Gtk
 from gi.repository import Geany
 from gi.repository import Peasy
+from gi.repository import GLib
 
 _ = Peasy.gettext
 
+available_formatters = []
+available_linters = []
+DEFAULT_FORMATTER = None
+DEFAULT_LINTER = None
+DEFAULT_LINE_WIDTH = 79
 
-def is_mod_available(modname, alternatives):
+
+def is_mod_available(modname):
     try:
         importlib.import_module(modname)
     except ImportError:
-        try:
-            return is_mod_available(alternatives[0], alternatives[1:])
-        except Exception as exc:
-            print(exc)
-            return False, ""
+        return False
     else:
-        return True, modname
+        return True
 
 
-IS_FORMATTER_AVAILABLE, name = is_mod_available("black", ["autopep8", "yapf"])
-if IS_FORMATTER_AVAILABLE:
-    print("Formatter: {}".format(name))
+while True:
+    for formatter in ["black", "autopep8", "yapf"]:
+        if is_mod_available(formatter):
+            available_formatters.append(formatter)
+            if not DEFAULT_FORMATTER:
+                DEFAULT_FORMATTER = formatter
+    for linter in ["flake8", "pylint", "pyflakes", "pycodestyle"]:
+        if is_mod_available(linter):
+            available_linters.append(linter)
+            if not DEFAULT_LINTER:
+                DEFAULT_LINTER = linter
+    break
 
-    def get_formatter(name=name):
+if DEFAULT_FORMATTER:
+    print("Formatter: {}".format(DEFAULT_FORMATTER))
+
+    def get_formatter(name=DEFAULT_FORMATTER):
         if name == "black":
             import black
 
@@ -37,7 +52,7 @@ if IS_FORMATTER_AVAILABLE:
             def FormatCode(content, style_config=None):
                 try:
                     changed_content = black.format_file_contents(
-                        content, line_length=79, fast=False
+                        content, line_length=style_config["line_width"], fast=False
                     )
                 except black.NothingChanged:
                     return "", False
@@ -46,9 +61,7 @@ if IS_FORMATTER_AVAILABLE:
 
             return FormatCode, GetDefaultStyleForDir
         elif name == "yapf":
-            from yapf.yapflib.yapf_api import (
-                FormatCode
-            )  # reformat a string of code
+            from yapf.yapflib.yapf_api import FormatCode  # reformat a string of code
             from yapf.yapflib.file_resources import GetDefaultStyleForDir
 
             return FormatCode, GetDefaultStyleForDir
@@ -58,7 +71,7 @@ if IS_FORMATTER_AVAILABLE:
             GetDefaultStyleForDir = None
 
             def FormatCode(content, style=None):
-                return fix_code(content)
+                return fix_code(content, options={"max_line_length": style["line_width"]})
 
             return FormatCode, GetDefaultStyleForDir
         return None, None
@@ -67,19 +80,13 @@ if IS_FORMATTER_AVAILABLE:
 else:
     code_formatter = None
     default_style_dir = None
-IS_LINTER_AVAILABLE, name = is_mod_available(
-    "flake8", ["pylint", "pycodestyle", "pyflakes"]
-)
-if IS_LINTER_AVAILABLE:
-    print("Linter: {}".format(name))
+if DEFAULT_LINTER:
+    print("Linter: {}".format(DEFAULT_LINTER))
 
-    def get_patched_checker(name=name):
+    def get_patched_checker(name=DEFAULT_LINTER):
         def get_show_msg(err_code, msg, line, col):
             return "{}: [{},{}] {}".format(
-                "ERROR" if err_code.startswith("E9") else "WARNING",
-                line,
-                col,
-                msg,
+                "ERROR" if err_code.startswith("E9") else "WARNING", line, col, msg
             )
 
         def read_file_line(fd):
@@ -110,12 +117,10 @@ if IS_LINTER_AVAILABLE:
                 file_content = bytes(file_content) if file_content else b""
                 py_file = io.BytesIO(file_content)
                 flake8_mngr = sg._file_checker_manager
+                flake8_mngr.options.max_line_length = DEFAULT_LINE_WIDTH
                 checks = flake8_mngr.checks.to_dictionary()
                 file_chk = PatchedFileChecker(
-                    filename,
-                    checks,
-                    flake8_mngr.options,
-                    file_lines=read_file_line(py_file),
+                    filename, checks, flake8_mngr.options, file_lines=read_file_line(py_file)
                 )
                 file_chk.run_checks()
                 g = sg._application.guide
@@ -141,6 +146,7 @@ if IS_LINTER_AVAILABLE:
                 file_content = bytes(file_content) if file_content else b""
                 py_file = io.BytesIO(file_content)
                 chk = Checker(filename, lines=read_file_line(py_file))
+                chk.max_line_length = DEFAULT_LINE_WIDTH
                 results = chk.check_all()
                 results = chk.report._deferred_print
                 if not results:
@@ -167,12 +173,7 @@ if IS_LINTER_AVAILABLE:
 
                 def flake(self, message):
                     self.errors.append(
-                        (
-                            "",
-                            message.lineno,
-                            message.col,
-                            message.message % message.message_args,
-                        )
+                        ("", message.lineno, message.col, message.message % message.message_args)
                     )
 
             def check_and_get_results(filename, file_content=None):
@@ -203,9 +204,7 @@ if IS_LINTER_AVAILABLE:
                         if not msg.may_be_emitted():
                             self._msgs_state[msg.msgid] = False
                     basename = (
-                        os.path.splitext(os.path.basename(filename))[0]
-                        if filename
-                        else "untitled"
+                        os.path.splitext(os.path.basename(filename))[0] if filename else "untitled"
                     )
                     walker = utils.PyLintASTWalker(self)
                     self.config.reports = True
@@ -213,35 +212,24 @@ if IS_LINTER_AVAILABLE:
                     tokencheckers = [
                         c
                         for c in _checkers
-                        if interfaces.implements(c, interfaces.ITokenChecker)
-                        and c is not self
+                        if interfaces.implements(c, interfaces.ITokenChecker) and c is not self
                     ]
                     rawcheckers = [
-                        c
-                        for c in _checkers
-                        if interfaces.implements(c, interfaces.IRawChecker)
+                        c for c in _checkers if interfaces.implements(c, interfaces.IRawChecker)
                     ]
                     # notify global begin
                     for checker in _checkers:
                         checker.open()
-                        if interfaces.implements(
-                            checker, interfaces.IAstroidChecker
-                        ):
+                        if interfaces.implements(checker, interfaces.IAstroidChecker):
                             walker.add_checker(checker)
                     self.set_current_module(basename, filename)
-                    ast_node = bd.string_build(
-                        file_content, filename, basename
-                    )
+                    ast_node = bd.string_build(file_content, filename, basename)
                     self.file_state = utils.FileState(basename)
                     self._ignore_file = False
                     # fix the current file (if the source file was not available or
                     # if it's actually a c extension)
-                    self.current_file = (
-                        ast_node.file
-                    )  # pylint: disable=maybe-no-member
-                    self.check_astroid_module(
-                        ast_node, walker, rawcheckers, tokencheckers
-                    )
+                    self.current_file = ast_node.file  # pylint: disable=maybe-no-member
+                    self.check_astroid_module(ast_node, walker, rawcheckers, tokencheckers)
                     # warn about spurious inline messages handling
                     spurious_messages = self.file_state.iter_spurious_suppression_messages(
                         self.msgs_store
@@ -255,9 +243,7 @@ if IS_LINTER_AVAILABLE:
 
             def check_and_get_results(filename, file_content=None):
                 if not isinstance(file_content, str):
-                    file_content = (
-                        file_content.decode("utf8") if file_content else ""
-                    )
+                    file_content = file_content.decode("utf8") if file_content else ""
                 if not filename:
                     filename = ""
                 linter = PatchedPyLinter()
@@ -266,9 +252,7 @@ if IS_LINTER_AVAILABLE:
                 linter.set_reporter(rp)
                 linter.check(filename, file_content)
                 for msg in rp.messages:
-                    yield msg[
-                        "line"
-                    ], "{type}: [{line},{column}] ({message-id}) {message}".format(
+                    yield msg["line"], "{type}: [{line},{column}] ({message-id}) {message}".format(
                         **msg
                     )
 
@@ -279,8 +263,7 @@ if IS_LINTER_AVAILABLE:
 
 def check_python_code(doc, filename, file_content):
     checks = sorted(
-        check_and_get_results(filename, file_content.encode("utf8")),
-        key=operator.itemgetter(0),
+        check_and_get_results(filename, file_content.encode("utf8")), key=operator.itemgetter(0)
     )
     error = False
     for line, msg in checks:
@@ -289,7 +272,7 @@ def check_python_code(doc, filename, file_content):
     return error
 
 
-class PyCheckPlugin(Peasy.Plugin):
+class PyCheckPlugin(Peasy.Plugin, Peasy.PluginConfigure):
     __gtype_name__ = "peasypycheck"
     item = None
     keys = None
@@ -297,21 +280,28 @@ class PyCheckPlugin(Peasy.Plugin):
 
     def do_enable(self):
         geany_data = self.geany_plugin.geany_data
-        self.item = Geany.ui_image_menu_item_new(
-            Gtk.STOCK_EXECUTE, _("Format Python Code")
-        )
+        self.item = Geany.ui_image_menu_item_new(Gtk.STOCK_EXECUTE, _("Format Python Code"))
         self.item.connect("activate", self.on_item_click)
         geany_data.main_widgets.tools_menu.append(self.item)
         self.item.show_all()
         self.keys = self.add_key_group("python_code_format", 1)
-        self.keys.add_keybinding(
-            "python_code_format", _("Python Code Format"), self.item, 0, 0
-        )
+        self.keys.add_keybinding("python_code_format", _("Python Code Format"), self.item, 0, 0)
         self.set_signal_handler()
+        # load startup config
+        self.keyfile = GLib.KeyFile.new()
+        global DEFAULT_FORMATTER
+        global DEFAULT_LINTER
+        global DEFAULT_LINE_WIDTH
+        self.pycheck_config = os.path.join(geany_data.app.configdir, "plugins/pycheck.conf")
+        if os.path.isfile(self.pycheck_config):
+            self.keyfile.load_from_file(self.pycheck_config, GLib.KeyFileFlags.KEEP_COMMENTS)
+            DEFAULT_FORMATTER = self.keyfile.get_string("pycheck", "formatter")
+            DEFAULT_LINTER = self.keyfile.get_string("pycheck", "linter")
+            DEFAULT_LINE_WIDTH = self.keyfile.get_integer("pycheck", "line-width")
         return True
 
     def set_signal_handler(self):
-        if not IS_LINTER_AVAILABLE:
+        if not DEFAULT_LINTER:
             return
         o = self.geany_plugin.geany_data.object
         signals = (
@@ -325,10 +315,7 @@ class PyCheckPlugin(Peasy.Plugin):
             self.handlers.append(o.connect(sig, self.on_document_notify))
 
     def on_document_notify(self, user_data, doc):
-        if (
-            not IS_LINTER_AVAILABLE
-            or doc.file_type.id != Geany.FiletypeID.FILETYPES_PYTHON
-        ):
+        if not DEFAULT_LINTER or doc.file_type.id != Geany.FiletypeID.FILETYPES_PYTHON:
             return
         filename = doc.real_path or doc.file_name
         sci = doc.editor.sci
@@ -338,9 +325,7 @@ class PyCheckPlugin(Peasy.Plugin):
         Geany.msgwin_clear_tab(Geany.MessageWindowTabNum.MESSAGE)
         error = False
         with ThreadPoolExecutor(max_workers=2) as executor:
-            error = executor.submit(
-                check_python_code, doc, filename, file_content
-            ).result()
+            error = executor.submit(check_python_code, doc, filename, file_content).result()
         if error:
             Geany.msgwin_switch_tab(Geany.MessageWindowTabNum.MESSAGE, False)
 
@@ -348,10 +333,7 @@ class PyCheckPlugin(Peasy.Plugin):
         if not code_formatter:
             return
         cur_doc = Geany.document_get_current()
-        if (
-            not cur_doc
-            or cur_doc.file_type.id != Geany.FiletypeID.FILETYPES_PYTHON
-        ):
+        if not cur_doc or cur_doc.file_type.id != Geany.FiletypeID.FILETYPES_PYTHON:
             return
         sci = cur_doc.editor.sci
         contents = sci.get_contents(-1)
@@ -363,6 +345,9 @@ class PyCheckPlugin(Peasy.Plugin):
             project = self.geany_plugin.geany_data.app.project
             if project and not style:
                 style = default_style_dir(project.base_path)
+            style["COLUMN_LIMIT"] = DEFAULT_LINE_WIDTH
+        else:
+            style = {"line_width": DEFAULT_LINE_WIDTH}
         format_text, formatted = code_formatter(contents, style_config=style)
         if formatted:
             sci.set_text(format_text)
@@ -381,3 +366,63 @@ class PyCheckPlugin(Peasy.Plugin):
             o = self.geany_plugin.geany_data.object
             for h in self.handlers:
                 o.disconnect(h)
+
+    def on_configure_response(self, dlg, response_id, user_data):
+        if response_id not in (Gtk.ResponseType.APPLY, Gtk.ResponseType.OK):
+            return
+        global DEFAULT_FORMATTER
+        global DEFAULT_LINTER
+        global DEFAULT_LINE_WIDTH
+        for child in user_data.get_children():
+            if isinstance(child, Gtk.Label):
+                continue
+            try:
+                val = child.get_active_text()
+            except AttributeError:
+                val = child.get_value_as_int()
+                DEFAULT_LINE_WIDTH = val
+            if val in available_formatters:
+                DEFAULT_FORMATTER = val
+            elif val in available_linters:
+                DEFAULT_LINTER = val
+        if os.path.isfile(self.pycheck_config):
+            self.keyfile.load_from_file(self.pycheck_config, GLib.KeyFileFlags.KEEP_COMMENTS)
+        self.keyfile.set_string("pycheck", "formatter", DEFAULT_FORMATTER)
+        self.keyfile.set_string("pycheck", "linter", DEFAULT_LINTER)
+        self.keyfile.set_integer("pycheck", "line-width", DEFAULT_LINE_WIDTH)
+        self.keyfile.save_to_file(self.pycheck_config)
+
+    def do_configure(self, dialog):
+        align = Gtk.Alignment.new(0, 0, 1, 0)
+        align.props.left_padding = 12
+        vbox = Gtk.VBox(orientation=Gtk.Orientation.VERTICAL, spacing=2)
+        vbox.set_border_width(2)
+        listbox = Gtk.ListStore(str)
+        listbox.set_data
+        label = Gtk.Label(_("Formatter"))
+        label.set_alignment(0, 0.5)
+        entry = Gtk.ComboBoxText()
+        for f in available_formatters:
+            entry.append_text(f)
+        entry.set_active(available_formatters.index(DEFAULT_FORMATTER))
+        vbox.add(label)
+        vbox.add(entry)
+        label = Gtk.Label(_("Linter"))
+        label.set_alignment(0, 0.5)
+        entry = Gtk.ComboBoxText()
+        for l in available_linters:
+            entry.append_text(l)
+        entry.set_active(available_linters.index(DEFAULT_LINTER))
+        vbox.add(label)
+        vbox.add(entry)
+        label = Gtk.Label(_("Line Width"))
+        label.set_alignment(0, 0.5)
+        adj = Gtk.Adjustment(DEFAULT_LINE_WIDTH, 79, 150, 1, 0, 0)
+        entry = Gtk.SpinButton()
+        entry.set_adjustment(adj)
+        entry.set_value(DEFAULT_LINE_WIDTH)
+        vbox.add(label)
+        vbox.add(entry)
+        align.add(vbox)
+        dialog.connect("response", self.on_configure_response, vbox)
+        return align
